2c568886ca88dc011da4fb53dadb9849e1dc1bf1
Corey Burrows <corey.burrows@centro.net>
2015-03-08 22:40:37 -0500

    Don't export the router object.

--- dist/statechart.js
+++ dist/statechart.js
@@ -47,8 +47,7 @@ this["statechart"] =
 
 	module.exports = {
 	  State: __webpack_require__(1),
-	  Router: __webpack_require__(2),
-	  RoutableState: __webpack_require__(3),
+	  RoutableState: __webpack_require__(2),
 	};
 
 
@@ -878,6 +877,80 @@ this["statechart"] =
 	(function() {
 	  "use strict";
 
+	  var State = __webpack_require__(1), router = __webpack_require__(3), util = __webpack_require__(4);
+
+	  function RoutableState() {
+	    State.apply(this, arguments);
+	  }
+
+	  util.assign(RoutableState, State);
+
+	  RoutableState.prototype = Object.create(State.prototype);
+	  RoutableState.prototype.constructor = RoutableState;
+
+	  RoutableState.prototype.route = function(pattern, opts) {
+	    var _this = this;
+
+	    if (this.__route__) {
+	      throw new Error("RoutableState#route: a route has already been defined on " + this);
+	    }
+
+	    this.__route__ = router.define(pattern, function(params) {
+	      _this.root().goto(_this.path(), {force: true, context: params});
+	    }, opts);
+
+	    this.enter(function(ctx) {
+	      router.route(this.__route__);
+	      router.params(util.pick(ctx || {}, this.__route__.names));
+	    });
+
+	    this.exit(function(ctx) {
+	      var params = {}, names = this.__route__.names, i, n;
+
+	      ctx = ctx || {};
+
+	      for (i = 0, n = names.length; i < n; i++) {
+	        if (!(names[i] in ctx)) {
+	          params[names[i]] = undefined;
+	        }
+	      }
+
+	      router.params(params);
+	    });
+
+	    return this;
+	  };
+
+	  RoutableState.prototype.start = function(opts) {
+	    router.start(opts);
+	    return this;
+	  };
+
+	  RoutableState.prototype.stop = function() {
+	    router.stop();
+	    return this;
+	  };
+
+	  RoutableState.prototype.params = function() {
+	    return router.params.apply(router, arguments);
+	  };
+
+	  RoutableState.prototype.unknown = function(f) {
+	    router.unknown(f);
+	    return this;
+	  };
+
+	  module.exports = RoutableState;
+	}());
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function() {
+	  "use strict";
+
 	  var queryString      = __webpack_require__(6),
 	      equals           = __webpack_require__(5),
 	      util             = __webpack_require__(4),
@@ -1079,80 +1152,6 @@ this["statechart"] =
 	  };
 
 	  module.exports = new Router();
-	}());
-
-
-/***/ },
-/* 3 */
-/***/ function(module, exports, __webpack_require__) {
-
-	(function() {
-	  "use strict";
-
-	  var State = __webpack_require__(1), router = __webpack_require__(2), util = __webpack_require__(4);
-
-	  function RoutableState() {
-	    State.apply(this, arguments);
-	  }
-
-	  util.assign(RoutableState, State);
-
-	  RoutableState.prototype = Object.create(State.prototype);
-	  RoutableState.prototype.constructor = RoutableState;
-
-	  RoutableState.prototype.route = function(pattern, opts) {
-	    var _this = this;
-
-	    if (this.__route__) {
-	      throw new Error("RoutableState#route: a route has already been defined on " + this);
-	    }
-
-	    this.__route__ = router.define(pattern, function(params) {
-	      _this.root().goto(_this.path(), {force: true, context: params});
-	    }, opts);
-
-	    this.enter(function(ctx) {
-	      router.route(this.__route__);
-	      router.params(util.pick(ctx || {}, this.__route__.names));
-	    });
-
-	    this.exit(function(ctx) {
-	      var params = {}, names = this.__route__.names, i, n;
-
-	      ctx = ctx || {};
-
-	      for (i = 0, n = names.length; i < n; i++) {
-	        if (!(names[i] in ctx)) {
-	          params[names[i]] = undefined;
-	        }
-	      }
-
-	      router.params(params);
-	    });
-
-	    return this;
-	  };
-
-	  RoutableState.prototype.start = function(opts) {
-	    router.start(opts);
-	    return this;
-	  };
-
-	  RoutableState.prototype.stop = function() {
-	    router.stop();
-	    return this;
-	  };
-
-	  RoutableState.prototype.params = function() {
-	    return router.params.apply(router, arguments);
-	  };
-
-	  RoutableState.prototype.unknown = function(f) {
-	    router.unknown(f);
-	    return this;
-	  };
-
-	  module.exports = RoutableState;
 	}());
 
 
