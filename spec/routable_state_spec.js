99eb46e299fead6ae96e829bb0536eb904d8993a
Corey Burrows <corey.burrows@centro.net>
2015-03-11 13:02:49 -0500

    Updates RoutableState#route to accept the destination state.

--- spec/routable_state_spec.js
+++ spec/routable_state_spec.js
@@ -45,6 +45,14 @@ describe('RoutableState', function() {
         state.route('/abc');
       }).toThrow(new Error('RoutableState#route: a route has already been defined on ' + state));
     });
+
+    it('throws an exception when given a state string that does not resolve', function() {
+      expect(function() {
+        RoutableState.define(function() {
+          this.route('/foo/bar', './foo/bar');
+        });
+      }).toThrow(new Error('RoutableState#route: invalid state: ./foo/bar'));
+    });
   });
 
   describe('on popstate events', function() {
@@ -56,6 +64,36 @@ describe('RoutableState', function() {
       router._handleLocationChange('/foos/12', '');
       router.flush();
       expect(this.statechart.current()).toEqual(['/show']);
+    });
+
+    describe('with routes defined with a state string', function() {
+      beforeEach(function() {
+        this.statechart = RoutableState.define(function() {
+          this.state('start');
+          this.state('A');
+          this.state('B');
+          this.state('C');
+
+          this.route('/a', './A');
+          this.route('/b', './B');
+          this.route('/c', './C');
+        });
+
+        this.statechart.goto('/start');
+      });
+
+      it('triggers a transition to the state with the matching route', function() {
+        expect(this.statechart.current()).toEqual(['/start']);
+        router._handleLocationChange('/a', '');
+        router.flush();
+        expect(this.statechart.current()).toEqual(['/A']);
+        router._handleLocationChange('/b', '');
+        router.flush();
+        expect(this.statechart.current()).toEqual(['/B']);
+        router._handleLocationChange('/c', '');
+        router.flush();
+        expect(this.statechart.current()).toEqual(['/C']);
+      });
     });
   });
 
