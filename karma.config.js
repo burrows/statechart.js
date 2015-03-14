d22d664128342cedabc1b3cb259c7bfcc7c553e0
Corey Burrows <corey.burrows@centro.net>
2015-03-14 17:08:11 -0500

    Adds karma html reporter.

--- karma.config.js
+++ karma.config.js
@@ -31,7 +31,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'html'],
 
 
     // web server port
@@ -58,7 +58,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: false,
 
 
     webpack: {
